438b591724f8ab61ee271f229a80a4a39f8a83a3
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2026-04-05 01:41:03 -0600

    fix: remove double import from connect.js file

--- movies-app-backend/src/db/connect.js
+++ movies-app-backend/src/db/connect.js
@@ -1,7 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const { Sequelize } = require('sequelize');
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
